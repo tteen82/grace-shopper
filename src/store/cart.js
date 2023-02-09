29d2972f9c3003500a735c77a3980c84a557b62f
Samuel Labastille <samuellabastille17@gmail.com>
2023-02-08 20:21:39 -0500

    reducers for cart complete

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,23 +1,61 @@
 import axios from 'axios';
-const cart = (state = { lineItems: [] }, action)=> {
-  if(action.type === 'SET_CART'){
-    return action.cart;
+const cart = (state = { lineItems: [] }, action) => {
+  if (action.type === 'SET_CART') {
+    return { ...state, lineItems: action.cart };
+  }
+  if (action.type === 'ADD_CART') {
+    return { ...state, lineItems: [...state.lineItems, action.cart] };
+  }
+  if (action.type === 'DELETE_CART') {
+    const newLineItems = state.lineItems.filter(
+      (item) => item.id !== action.item.id
+    );
+    return { ...state, lineItems: [...newLineItems] };
   }
   return state;
 };
 
-
-export const fetchCart = ()=> {
-  return async(dispatch)=> {
+//get the full cart
+export const fetchCart = () => {
+  return async (dispatch) => {
     const token = window.localStorage.getItem('token');
     const response = await axios.get('/api/orders/cart', {
       headers: {
-        authorization: token
-      }
+        authorization: token,
+      },
     });
     dispatch({ type: 'SET_CART', cart: response.data });
   };
 };
 
+//add to the cart
+export const addToCart = (product, quantity) => {
+  return async (dispatch) => {
+    const token = window.localStorage.getItem('token');
+    const response = await axios.post('/api/orders/cart/', {
+      headers: {
+        authorization: token,
+      },
+      product,
+      quantity,
+    });
+    dispatch({ type: 'ADD_CART', cart: response.data });
+  };
+};
+
+//Remove from the cart
+export const removeFromCart = (product, quantityToRemove) => {
+  return async (dispatch) => {
+    const token = window.localStorage.getItem('token');
+    const response = await axios.put('/api/orders/cart/', {
+      headers: {
+        authorization: token,
+      },
+      product,
+      quantityToRemove,
+    });
+    dispatch({ type: 'DELETE_CART', cart: response.data });
+  };
+};
 
 export default cart;
